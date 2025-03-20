42e9f485e076935b7a58ae5b67541542f00f975d
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-03-20 18:25:36 -0500

    update result list

--- src/app/mm-results/mm-result-list/mm-result-list.component.ts
+++ src/app/mm-results/mm-result-list/mm-result-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtItem } from '../../shared/model/art-item.model';
+import { ArtObjectResult } from '../../shared/model/art-object-result.model';
 import { ArtService } from '../../shared/services/art.service';
 
 @Component({
@@ -9,9 +10,14 @@ import { ArtService } from '../../shared/services/art.service';
   styleUrl: './mm-result-list.component.css',
 })
 export class MmResultListComponent implements OnInit {
+  artObjectResult: ArtObjectResult;
   artItems: ArtItem[];
 
-  constructor(private artService: ArtService) {}
+  constructor(private artService: ArtService) {
+    this.artService.artItemsChanged.subscribe((updatedArtItems) => {
+      this.artItems = updatedArtItems;
+    });
+  }
 
   ngOnInit(): void {
     this.artItems = this.artService.getArtItems();
