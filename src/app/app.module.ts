08dc266f3721a300cd33244ea01a62db8ffc0649
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-03-20 15:08:57 -0500

    update search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MmAuthComponent } from './mm-auth/mm-auth.component';
 import { MmHeaderComponent } from './mm-header/mm-header.component';
+import { MmResultDetailComponent } from './mm-results/mm-result-detail/mm-result-detail.component';
 import { MmResultItemComponent } from './mm-results/mm-result-list/mm-result-item/mm-result-item.component';
 import { MmResultListComponent } from './mm-results/mm-result-list/mm-result-list.component';
 import { MmResultsComponent } from './mm-results/mm-results.component';
 import { MmSearchComponent } from './mm-search/mm-search.component';
-import { MmResultDetailComponent } from './mm-results/mm-result-detail/mm-result-detail.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { MmResultDetailComponent } from './mm-results/mm-result-detail/mm-result
     MmResultsComponent,
     MmResultDetailComponent,
   ],
-  imports: [BrowserModule, CommonModule],
+  imports: [BrowserModule, CommonModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/mm-search/mm-search.component.ts
+++ src/app/mm-search/mm-search.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-mm-search',
   standalone: false,
   templateUrl: './mm-search.component.html',
-  styleUrl: './mm-search.component.css'
+  styleUrl: './mm-search.component.css',
 })
 export class MmSearchComponent {
+  @ViewChild('idQuery') idQuery: ElementRef;
+  @ViewChild('titleQuery') titleQuery: ElementRef;
+  @ViewChild('departmentFilter') departmentFilter: ElementRef;
 
+  onSubmit() {
+    console.log(
+      `ID: ${this.idQuery.nativeElement.value}, Title: ${this.titleQuery.nativeElement.value}, Department Filter: ${this.departmentFilter.nativeElement.value}`
+    );
+  }
 }
