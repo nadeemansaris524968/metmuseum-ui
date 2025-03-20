61a93c514ed838962738b19b61df380de0999ffa
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-03-20 18:24:10 -0500

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MmAuthComponent } from './mm-auth/mm-auth.component';
 import { MmHeaderComponent } from './mm-header/mm-header.component';
@@ -9,7 +10,28 @@ import { MmResultDetailComponent } from './mm-results/mm-result-detail/mm-result
 import { MmResultItemComponent } from './mm-results/mm-result-list/mm-result-item/mm-result-item.component';
 import { MmResultListComponent } from './mm-results/mm-result-list/mm-result-list.component';
 import { MmResultsComponent } from './mm-results/mm-results.component';
-import { MmSearchComponent } from './mm-search/mm-search.component';
+import { MmSearchComponent } from './mm-results/mm-search/mm-search.component';
+import { AboutComponent } from './shared/links/about/about.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/art-results',
+    pathMatch: 'full',
+  },
+  {
+    path: 'art-results',
+    component: MmResultsComponent,
+  },
+  {
+    path: 'art-detail',
+    component: MmResultDetailComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +44,12 @@ import { MmSearchComponent } from './mm-search/mm-search.component';
     MmResultsComponent,
     MmResultDetailComponent,
   ],
-  imports: [BrowserModule, CommonModule, FormsModule],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
