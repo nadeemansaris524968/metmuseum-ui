204f42c1bb695457bda6ccb6724530f90458f5bc
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-03-21 11:04:23 -0500

    saving current page number to localStorage

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { forkJoin, Observable, switchMap } from 'rxjs';
 import { ArtItem } from '../shared/model/art-item.model';
 import { ArtService } from '../shared/services/art.service';
@@ -9,14 +9,23 @@ import { ArtService } from '../shared/services/art.service';
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css',
 })
-export class GalleryComponent {
-  currentPage = 1;
+export class GalleryComponent implements OnInit {
+  currentPage: number;
   paginatedArtItems$: Observable<ArtItem[]> = new Observable();
 
   constructor(private artService: ArtService) {
     this.artService.fetchArtObjectsIDs().subscribe((_) => {
       this.loadPage();
     });
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('currentPageNumber')) {
+      this.currentPage = parseInt(localStorage.getItem('currentPageNumber'));
+    } else {
+      this.currentPage = 1;
+      localStorage.setItem('currentPageNumber', this.currentPage.toString());
+    }
   }
 
   loadPage() {
@@ -34,11 +43,13 @@ export class GalleryComponent {
 
   incrementPageNumber() {
     this.currentPage += 1;
+    localStorage.setItem('currentPageNumber', this.currentPage.toString());
     this.loadPage();
   }
 
   decrementPageNumber() {
     this.currentPage -= 1;
+    localStorage.setItem('currentPageNumber', this.currentPage.toString());
     this.loadPage();
   }
 }
