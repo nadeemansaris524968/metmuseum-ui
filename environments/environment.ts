ca5c3e2cd4a94b17665b2da90a185632df2c8d27
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-03-23 14:09:35 -0500

    update environment urls

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  objectIDsURL:
-    'https://collectionapi.metmuseum.org/public/collection/v1/objects',
+  objectURL: 'https://collectionapi.metmuseum.org/public/collection/v1/objects',
   departmentsURL:
     'https://collectionapi.metmuseum.org/public/collection/v1/departments',
   searchURL: 'https://collectionapi.metmuseum.org/public/collection/v1/search',
