4045b844994ddf9f5d2457cdbb65ed7a6c50d8d5
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-03-26 23:21:14 -0500

    refactor

--- src/app/art-detail/art-detail.component.ts
+++ src/app/art-detail/art-detail.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { ArtItem } from '../shared/model/art-item.model';
 
 @Component({
@@ -9,19 +9,12 @@ import { ArtItem } from '../shared/model/art-item.model';
   templateUrl: './art-detail.component.html',
   styleUrl: './art-detail.component.css',
 })
-export class ArtDetailComponent implements OnInit, OnDestroy {
-  private routeDataSub: Subscription;
-  artItem: ArtItem;
+export class ArtDetailComponent implements OnInit {
+  artItem: Observable<ArtItem>;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.routeDataSub = this.route.data.subscribe((data) => {
-      this.artItem = data['artItem'];
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.routeDataSub.unsubscribe();
+    this.artItem = this.route.data.pipe(map((data) => data['artItem']));
   }
 }
